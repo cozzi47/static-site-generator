b9c4ae40d0be436aab996ed5e1b611f683eb2325
cozzi47 <cozzi47@hotmail.com>
2025-03-10 16:29:10 -0400

    add copystatic.py and main function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+from copystatic import copy_files
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    print(TextNode("hello world", TextType.BOLD))
+    print("Clearing public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    
+    print("Copying static directory...")
+    copy_files(dir_path_static, dir_path_public)
+
 
 if __name__ == "__main__":
     main()
